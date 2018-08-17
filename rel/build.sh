49d2a3987b67357f8621eabd112524cfd4a8c678
Ferd T-H <mononcqc@ferd.ca>
2018-08-17 00:26:25 -0400

    updating for more modern erlangs and rebar3

--- rel/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env escript
-%% -*- erlang -*-
-%%! -smp enable -noshell
-main(_) ->
-    {ok, Conf} = file:consult("rel.conf"),
-    {ok, Spec} = reltool:get_target_spec(Conf),
-    reltool:eval_target_spec(Spec, code:root_dir(), "blogerl").
